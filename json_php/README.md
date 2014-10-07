3a32bccb2a7c7b741c28d57722d49181ae8b01c6
Martin <Martin@Martin-PC>
2014-10-07 06:01:47 +0200

    innitial commit for serie Working with json and php

---
+++ json_php/README.md
@@ -0,0 +1,6 @@
+Working with JSON and PHP
+===================
+
+This video introduces working with JSON and PHP together, using the built-in json_encode function. In this tutorial, we create a short example of a calculator to submit, request and add two numbers together. We ajax for the results and return them in valid JSON format.
+
+https://www.youtube.com/watch?v=2qJT09LAh64
---
+++ json_php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Working with JSON and PHP</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    </head>
+    <body>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
