a019b039517f9373e52e23086dcd8919ba1d4509
Martin <Martin@Martin-PC>
2014-10-17 05:35:26 +0200

    CSRF script version 1.0

--- csrf/classes/Token.php
+++ csrf/classes/Token.php
@@ -5,12 +5,17 @@ class Token
 
     public static function generate()
     {
-	
+        return $_SESSION['rataja']['token'] = base64_encode(openssl_random_pseudo_bytes(32));
     }
 
     public static function check($token)
     {
-	
+        if (isset($_SESSION['rataja']['token']) && $token === $_SESSION['rataja']['token'])
+        {
+            unset($_SESSION['rataja']['token']);
+            return true;
+        }
+        return false;
     }
 
 }
--- csrf/index.php
+++ csrf/index.php
@@ -1,42 +1,39 @@
 <?php
 session_start();
 require_once './classes/Token.php';
-if (isset($_POST['quantity'], $_POST['product']))
+if (isset($_POST['quantity'], $_POST['product'], $_POST['token']))
 {
     $product = $_POST['product'];
     $quantity = $_POST['quantity'];
     if (!empty($product) && !empty($quantity))
     {
-	
+        if (Token::check($_POST['token']))
+        {
+            echo 'Token is valid';
+        } else
+        {
+            echo 'Token is not valid';
+        }
     }
 }
 ?>
 <!DOCTYPE>
 <html>
     <head>
-	<title>CSRF Protection</title>
+        <title>CSRF Protection</title>
     </head>
     <body>
-	<form action="" method="post">
-	    <div class="product">
-		<strong>A product</strong>
-		<div class="field">
-		    Quantity: <input type="text" name="quantity">
-		    <input type="submit" value="Order">
-		    <input type="hidden" name="product" value="1">
-		    <input type="hidden" name="token" value="">
-		</div>
-	    </div>
-	</form>
+        <form action="" method="post">
+            <div class="product">
+                <strong>A product</strong>
+                <div class="field">
+                    Quantity: <input type="text" name="quantity">
+                    <input type="submit" value="Order">
+                    <input type="hidden" name="product" value="1">
+                    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+                </div>
+            </div>
+        </form>
     </body>
 
 </html>
-
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
